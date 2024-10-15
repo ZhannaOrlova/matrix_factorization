e023117fcc31114340d74ee1885f5f6f212bcf11
Zhanna Orlova <orlova.zhan@gmail.com>
2024-10-14 21:21:28 -0600

    minor adjustments

--- infrastructure/rating_repository.py
+++ infrastructure/rating_repository.py
@@ -5,15 +5,13 @@ from dotenv import load_dotenv
 
 class RatingRepository:
     def __init__(self):
-        load_dotenv()  # Load environment variables from .env file
-        self.csv_file = os.getenv('CSV_FILE_PATH')  # Get the CSV file path from the environment variable
+        load_dotenv()  
+        self.csv_file = os.getenv('CSV_FILE_PATH')  
 
     def load_ratings(self):
-        # Load the CSV file from the specified path
         print("Loading CSV file...")
         df = pd.read_csv(self.csv_file)
 
-        # Check if expected columns are present
         required_columns = ['userId', 'movieId', 'rating']
         if not all(col in df.columns for col in required_columns):
             raise KeyError("Expected columns 'userId', 'movieId', 'rating' not found in DataFrame")
@@ -21,11 +19,11 @@ class RatingRepository:
         print(f"DataFrame shape: {df.shape}")
         print("Columns in DataFrame:", df.columns)
 
-        # Limit the DataFrame to the first 1,000 rows for testing
+        # Limit the DataFrame to the first 1,000 rows (it's too large)
         df = df.head(1000)
         print(f"Reduced DataFrame shape: {df.shape}")
 
-        # Select a sample of unique movie IDs
+        # Sample of unique movie IDs
         print("Sampling movie IDs...")
         unique_movie_ids = df['movieId'].unique()
         num_samples = min(10000, len(unique_movie_ids))
@@ -33,21 +31,20 @@ class RatingRepository:
         # Randomly sample movie IDs
         sample_movie_ids = pd.Series(unique_movie_ids).sample(n=num_samples, random_state=42)
 
-        # Filter the DataFrame to include only the selected movies
+        # Filter the DataFrame to include selected movies
         df_sampled = df[df['movieId'].isin(sample_movie_ids)]
         print(f"Sampled DataFrame shape: {df_sampled.shape}")
 
-        # Create a user-item ratings matrix from the sampled data
+        # User-item ratings matrix fromsampled data
         print("Creating ratings matrix...")
         ratings_matrix = df_sampled.pivot(index='userId', columns='movieId', values='rating').fillna(0).values
         
         print("Ratings matrix created successfully!")
         
-        # Create a mapping from userId to index
+        # Mapping from userId to index
         user_id_mapping = {user_id: index for index, user_id in enumerate(df_sampled['userId'].unique())}
         return ratings_matrix, user_id_mapping
 
-# Test the RatingRepository
 if __name__ == "__main__":
     repo = RatingRepository()
     try:
